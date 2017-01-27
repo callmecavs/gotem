47cf19e91f9da2e7330b31f2397d1aa0e5d9c632
Michael Cavalea <callmecavs@gmail.com>
2017-01-27 17:54:33 -0500

    first take

--- src/gotem.js
+++ src/gotem.js
@@ -1,5 +1,20 @@
-const gotem = () => {
+const gotem = target => new Promise((resolve, reject) => {
+  // create range, and select targets content
+  const range = document.createRange()
+  range.selectNodeContents(target)
 
-}
+  // remove existing selections, then add the new one
+  const selection = window.getSelection()
+  selection.removeAllRanges()
+  selection.addRange(range)
+
+  // execute the copy command
+  const success = document.execCommand('copy')
+
+  // return based on the result of trying to run the command
+  return success
+    ? resolve()
+    : reject()
+})
 
 export default gotem
