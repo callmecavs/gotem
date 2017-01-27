32f76819e2c9b8e50582a0a2c1533b5ded78df2f
Michael Cavalea <callmecavs@gmail.com>
2017-01-27 18:32:02 -0500

    rewrite

--- src/gotem.js
+++ src/gotem.js
@@ -1,20 +1,39 @@
-const gotem = target => new Promise((resolve, reject) => {
-  // create range, and select targets content
-  const range = document.createRange()
-  range.selectNodeContents(target)
+const gotem = (options = {}) => {
+  // throw if no target or no trigger
+  if (!options.target || !options.trigger) {
+    throw new Error('gotem: target and trigger parameters are required.')
+  }
 
-  // remove existing selections, then add the new one
-  const selection = window.getSelection()
-  selection.removeAllRanges()
-  selection.addRange(range)
+  const handle = event => {
+    // prevent default link behavior
+    // note that tagName returns in uppercase
+    if (event.target.tagName === 'A') {
+      event.preventDefault()
+    }
 
-  // execute the copy command
-  const success = document.execCommand('copy')
+    // create range, and select target node contents
+    const range = document.createRange()
+    range.selectNodeContents(options.target)
 
-  // return based on the result of trying to run the command
-  return success
-    ? resolve()
-    : reject()
-})
+    // remove existing selections, then add the new one
+    const selection = window.getSelection()
+    selection.removeAllRanges()
+    selection.addRange(range)
+
+    // execute the copy command
+    const executed = document.execCommand('copy')
+
+    // based on if the command executed
+    // check if the appropriate callback exists, and if it does, call it
+    if (executed) {
+      options.success && options.success()
+    } else {
+      options.error && options.error()
+    }
+  }
+
+  // bind the click handler to the trigger node
+  options.trigger.addEventListener('click', handle)
+}
 
 export default gotem
